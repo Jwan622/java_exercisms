5bc53116bbc07b2ff9b689cbe23b141dcd2bc29d
Jeffrey Wan <Jwan622@gmail.com>
2016-01-05 21:03:56 -0500

    adding etl exercsm

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+// this class will transform a Map<Integer, List<String>> into another Map in <String, Integer> format.
 public class Etl {
-   public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
-   }
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+    Map<String, Integer> myMap = new HashMap<String, Integer>();
+    for (Entry<Integer, List<String>> entry : old.entrySet()) {
+      int points = entry.getKey();
+      for (String oldLetter : entry.getValue()) {
+        String letter = oldLetter.toLowerCase();
+        myMap.put(letter, points);
+      }
+    }
+    return myMap;
+  }
 }
